ad99877415b44db31ac9ebe5d530bf277311e358
CamilleMj <camilleB512@hotmail.com>
2024-03-12 10:04:26 +0400

    DI Courses upload

---
+++ WEEK3/DAY6/DailyChallengeStars/script.js
@@ -0,0 +1,18 @@
+for (let i = 0; i <= 0; i++) {
+    console.log(" * ");
+    for (let j = 0; j <= 0; j++) {
+      console.log(" * * ");
+    }
+    for (let j = 0; j <= 0; j++) {
+      console.log(" * * * ");
+    }
+    for (let j = 0; j <= 0; j++) {
+      console.log(" * * * *");
+    }
+    for (let j = 0; j <= 0; j++) {
+      console.log(" * * * * *");
+    }
+    for (let j = 0; j <= 0; j++) {
+      console.log(" * * * * * *");
+    }
+  }
